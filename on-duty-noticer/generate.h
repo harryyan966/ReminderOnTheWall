d3627bb1340f57e23a8b9cdb1abd10a0de4590e7
ggstudy-ddup <825996020@qq.com>
2021-02-16 10:57:44 +0800

    Packed in a class.

--- on-duty-noticer/generate.h
+++ on-duty-noticer/generate.h
@@ -5,17 +5,43 @@
 using namespace cv;
 using namespace std;
 
-void GetStringSize(HDC hDC, const char* str, int* w, int* h);
-void putTextZH(Mat &dst, const char* str, Point org, Scalar color, int fontSize,
-    const char *fn = "Arial", bool italic = false, bool underline = false);
-
-inline int changeImage()
+namespace generate
 {
-    cv::Mat img = cv::imread("C:\\1.png", cv::ImreadModes::IMREAD_COLOR);
-    int font = cv::FONT_HERSHEY_DUPLEX;
-    putTextZH(img, "ΩÒÃÏ «°£°£°£", cv::Point(50, img.rows / 2 - 50), cv::Scalar(0, 0, 0), 72);
-    cv::imshow("img", img);
-    cv::waitKey();
-    return 0;
-}
+    // under the same dir or its sub dir only 
+    void change_wallpaper(string base, const char *fname);
 
+    class ChangeWallpaper
+    {
+        string fname;
+        cv::Mat *mat;
+        int font;
+
+        static void GetStringSize(HDC hDC, const char* str, int* w, int* h);
+    public:
+        ChangeWallpaper() : fname(""), mat(nullptr), font(cv::FONT_HERSHEY_DUPLEX) {}
+        ChangeWallpaper(const string &full_path);
+        ChangeWallpaper(const string &runtime_path, const string &file_rel_path);
+        ~ChangeWallpaper();
+
+        inline const cv::Mat& getMat(void) const { return *mat; }
+
+        void save(const string& argv, const string& img);
+        inline void putTextZH(const char* str, Point org, Scalar color, int fontSize,
+            const char* fn = "Arial", bool italic = false, bool underline = false) {
+            putTextZH(*mat, str, org, color, fontSize, fn, italic, underline);
+        }
+        inline void changeWallpaper(const string &argv, const string &img) {
+            string path = getBaseName(argv.c_str());
+            path += img;
+            SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0,
+                (PVOID)path.c_str(),
+                SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+
+        // get base name
+        static string getBaseName(const char* argv);
+        // put chinese character on cv::Mat
+        static void putTextZH(Mat& dst, const char* str, Point org, Scalar color, int fontSize,
+            const char* fn = "Arial", bool italic = false, bool underline = false);
+    };
+}

bafa846330437d6fa0fec0838192580ece7498ad
Joss Lei <josslei@163.com>
2021-07-15 15:33:43 +0800

    SimpleCSV writer part implemented, fixed the reader

--- src/static/Reminder-On-The-Wall_static/headers/SimpleCSV.h
+++ src/static/Reminder-On-The-Wall_static/headers/SimpleCSV.h
@@ -4,6 +4,7 @@
 using std::string;
 using std::fstream;
 using std::vector;
+using std::to_string;
 
 class SimpleCSV
 {
@@ -14,17 +15,46 @@ class SimpleCSV
 	vector<string>* chart;
 
 public:
-	SimpleCSV(string f, char mode);
+	SimpleCSV(string f);
 	~SimpleCSV() {
 		if (nullptr != chart) delete[] chart;
 	}
 
 	// accessors
-	int getTotalLines() const { return total_Lines; }
 	const vector<string>* getChart() const { return chart; }
+	int getTotalLines() const { return total_Lines; }
+	/* get the number of cell from a certain row
+	 * @param i index of a row
+	 * @ret amount of cells in this row (without the last vacant cell)
+	 */
+	int getRowLength(int i) const { return chart[i].size() - 1; }
+	/* get the value from a certain cell
+	 * @ret the value in the cell
+	 */
+	string getCell(int row, int column) const {
+		if (row > getTotalLines() || column > getRowLength(row))
+			throw string("Index Out of Range!\nWhen accessing the cell (") +
+					to_string(row) + "," + to_string(column) + ")\n";
+		return chart[row][column];
+	}
+
+	// mutators
+	/* set the selected cell to a certain value
+	 * @param val the value is to set;
+	 */
+	void setCell(int row, int column, string val)
+	{
+		if (row > getTotalLines() || column > getRowLength(row))
+			throw string("Index Out of Range!\nWhen accessing the cell (") +
+			to_string(row) + "," + to_string(column) + ")\n";
+		chart[row][column] = val;
+	}
+	/* update the file with data in the chart */
+	void writeContent();
 
 private:
 	int findLineNumber();
 	void fillInChart();
+	string chart2str(const vector<string>* cht);
 
 };
--- src/static/Reminder-On-The-Wall_static/src/SimpleCSV.cpp
+++ src/static/Reminder-On-The-Wall_static/src/SimpleCSV.cpp
@@ -4,41 +4,49 @@ using namespace std;
 
 #include <iostream>
 
-SimpleCSV::SimpleCSV(string f, char mode) : file_name(f), total_Lines(0), chart(nullptr)
+SimpleCSV::SimpleCSV(string f) : file_name(f), total_Lines(0), chart(nullptr)
 {
-	if ('r' == mode)
+	ifstream fp(f, ios::binary);
+	// find file length
+	fp.seekg(0, ios::end);
+	int flength = fp.tellg();
+	fp.seekg(0, ios::beg);
+	// read file
+	char buffer = 0;
+	int i = 0;
+	while (i < flength)
 	{
-		ifstream fp(f, ios::binary);
-		// find file length
-		fp.seekg(0, ios::end);
-		int flength = fp.tellg();
-		fp.seekg(0, ios::beg);
-		// read file
-		char buffer = 0;
-		int i = 0;
-		while (i < flength)
-		{
-			fp.read(&buffer, sizeof(char));
-			// LF only
-			if ('\r' == buffer) {
-				flength--;
-				continue;
-			}
-			content.push_back(buffer);
-			i++;
+		fp.read(&buffer, sizeof(char));
+		// LF only
+		if ('\r' == buffer) {
+			flength--;
+			continue;
 		}
-		// init chart
-		total_Lines = findLineNumber();
-		chart = new vector<string>[total_Lines];
-		chart[0].push_back("");
-		// fill the chart
-		fillInChart();
+		content.push_back(buffer);
+		i++;
 	}
-	else if ('w' == mode)
+	fp.close();
+	// init chart
+	total_Lines = findLineNumber();
+	if (0 == total_Lines && flength != 0)	// the special
+		total_Lines = 1;
+	chart = new vector<string>[total_Lines];
+	chart[0].push_back("");
+	// fill the chart
+	fillInChart();
+}
+
+void SimpleCSV::writeContent()
+{
+	content = chart2str(chart);
+	ofstream fp = ofstream(file_name, ios::binary);
+	for (int i = 0; i < content.size(); i++)
 	{
-		// pass
+		if ('\n' == content[i])
+			fp.write("\r", sizeof(char));
+		fp.write(&content[i], sizeof(char));
 	}
-	else throw "Unexpected mode!";
+	fp.close();
 }
 
 int SimpleCSV::findLineNumber()
@@ -86,4 +94,35 @@ void SimpleCSV::fillInChart()
 			break;
 		}
 	}
+}
+
+string SimpleCSV::chart2str(const vector<string>* cht)
+{
+	string str = "";
+	for (int i = 0; i < this->getTotalLines(); i++)
+	{
+		int row_length = cht[i].size();
+		// if there is an empty cell at the back
+		if (cht[i][cht[i].size() - 1].empty()) row_length--;	// discard the empty cell at the back
+		for (int j = 0; j < row_length; j++)
+		{
+			if (-1 != cht[i][j].find(",") || -1 != cht[i][j].find("\""))
+			{
+				str += "\"";
+				for (int k = 0; k < cht[i][j].size(); k++)
+				{
+					if ('"' == cht[i][j][k])
+						str += "\"\"";
+					else
+						str += cht[i][j][k];
+				}
+				str += "\"";
+			}
+			else
+				str += cht[i][j];
+			str += ",";
+		}
+		str += "\n";
+	}
+	return str;
 }
